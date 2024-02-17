2d4282d2fe5887ba5d89ff7c1a3749751755c5d2
vinicius.carmo <vinicius.carmo@ifood.com.br>
2024-02-17 17:15:03 -0300

    Adding project bsds05

---
+++ bds05-main 2/src/main/java/com/devsuperior/movieflix/entities/Genre.java
@@ -0,0 +1,52 @@
+package com.devsuperior.movieflix.entities;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
+import javax.persistence.Table;
+import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Table(name = "tb_genre")
+public class Genre implements Serializable {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+
+    @OneToMany(mappedBy = "genre")
+    private List<Movie> movies = new ArrayList<>();
+
+    public Genre() {
+    }
+
+    public Genre(Long id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public List<Movie> getMovies() {
+        return movies;
+    }
+}
